3b8d58c216a60cb97994b6e5776adbccb9989f7e
subramania bharathy <subramania.bharathy@compassitesinc.com>
2015-03-13 13:33:02 +0530

    freshdesk configuration

---
+++ app/config/freshdesk.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Fresh Desk API Information
+	|--------------------------------------------------------------------------
+
+	*/
+	// Domain URL
+	'domain' => 'https://compassnet.freshdesk.com',
+	// Access tocken
+	'token' => 'k0FfSA2BHE9Yxw22ww',
+	// Password
+	'password' => 'X',
+);
