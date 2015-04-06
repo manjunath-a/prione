17f8ceb46d1b59301397dd806a20361abaa0cdde
subramania bharathy <subramania.bharathy@compassitesinc.com>
2015-04-06 17:02:43 +0530

    bug fixes

--- app/views/admin/users/create_edit.blade.php
+++ app/views/admin/users/create_edit.blade.php
@@ -112,7 +112,7 @@
 		<!-- Form Actions -->
 		<div class="form-group">
 			<div class="col-md-offset-2 col-md-10">
-				<element class="btn-cancel close_popup">Cancel</element>
+				<element class="btn btn-default close_popup">Cancel</element>
 				<button type="reset" class="btn btn-default">Reset</button>
 				<button type="submit" class="btn btn-success">OK</button>
 			</div>
